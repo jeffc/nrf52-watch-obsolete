89c9c08f7bbee773b822a30a233448dfa84bbd73
Jeff Cooper <jeff@thejeffcooper.com>
2020-02-09 18:48:46 -0500

    fixed library issues, can now compile with dummy graphics lib

---
+++ src/main_native.cpp
@@ -0,0 +1,11 @@
+#ifdef NATIVE
+
+#include <iostream>
+#include <Adafruit_GFX_dummy_display.h>
+
+int main() {
+  std::cout << "Hello world" << std::endl ;
+  return 0;
+}
+
+#endif
--- src/main_nrf52.cpp
+++ src/main_nrf52.cpp
@@ -1,7 +1,12 @@
+#ifdef EMBEDDED
+
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
 
 void setup() {
 }
 
 void loop() {
 }
+
+#endif
